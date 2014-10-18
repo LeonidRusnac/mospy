bd5dedb59a6d966699c7175bd5426d400b8b739f
Leonid <leonidrusnac4@gmail.com>
2014-10-18 10:56:06 +0200

    Added user profile class

---
+++ user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__    = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
+__license__   = "MIT"
+__copyright__ = "Copyright 2014, Leonid Rusnac"
+__version__   = "0.1"
+
+class User(object):
+	'''Rappresents a user profile'''
+	def __init__(self):
+		pass
+
+	def logged(self):
+		'''Check if the user is logged in'''
+		pass
+
+	def login(self):
+		'''Log in if not logged'''
+		pass
+
+	def logout(self):
+		'''Log out if logged in'''
+		pass
+
+
