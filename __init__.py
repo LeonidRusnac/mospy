016e4926b6ea2004480594aa4d67a5a20e0d49d1
Leonid <leonidrusnac4@gmail.com>
2014-10-18 10:47:33 +0200

    The module definition

---
+++ __init__.py
@@ -0,0 +1,6 @@
+__author__    = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
+__license__   = "MIT"
+__copyright__ = "Copyright 2014, Leonid Rusnac"
+__version__   = "0.1"
+
+__all__ = [] # list of all classes
