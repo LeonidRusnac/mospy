92f38593b9e256cac55e7938731e7f6506503302
Leonid <leonidrusnac4@gmail.com>
2014-10-24 10:08:52 +0200

    getTrainLevel

--- pet.py
+++ pet.py
@@ -15,3 +15,11 @@ class Pet(object):
 		self.session = session
 		self.config = userConfig
 
+	def getTrainLevel(self):
+		#/petarena/train/2087233/
+		response = self.session.get(self.config['siteUrl']+'petarena/train/'+str(self.petid)+'/')
+		if response.url == (self.config['siteUrl']+'petarena/train/'+str(self.petid)+'/'):
+			tree = html.fromstring(response.text)
+			print tree.xpath('//span[@class="num"]/text()')
+		else:
+			print 'Error getTrainLevel, be sure not blocked somewhere!'
