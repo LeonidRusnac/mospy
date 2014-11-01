f52b142e9830718c74c7c966043634000816ef34
Leonid <leonidrusnac4@gmail.com>
2014-11-02 00:36:49 +0100

    play with gypsy

--- gypsy.py
+++ gypsy.py
@@ -10,7 +10,27 @@ import requests
 from lxml import html
 
 class Gypsy(object):
-	def __init__(self, session, userConfig, petid):
-		self.petid = petid
+	def __init__(self, session, userConfig):
 		self.session = session
 		self.config = userConfig
+
+	def gypsyPlay(self):
+		response = self.session.get(self.config['siteUrl']+'camp/gypsy/')
+		if response.url == (self.config['siteUrl']+'camp/gypsy/'):
+			self.session.post(self.config['siteUrl']+'camp/gypsy//', data={
+				'action': 'gypsyStart',
+				'gametype': 1
+			})
+
+			print "Gypsy start!"
+
+			self.session.post(self.config['siteUrl']+'camp/gypsy//', data={
+				'action': 'gypsyAuto'
+			})
+
+			print "Gypsy done!"
+
+
+
+		else:
+			print 'Error play with gypsy, be sure not blocked somewhere!'
