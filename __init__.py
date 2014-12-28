606c4a8f93cdffccc12a180e4cfd7f721c6f60a5
Leonid <leonidrusnac4@gmail.com>
2014-12-28 18:15:12 +0100

    sovet register adder

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-__author__    = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
-__license__   = "MIT"
+__author__  = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
+__license__ = "MIT"
 __copyright__ = "Copyright 2014, Leonid Rusnac"
-__version__   = "0.1"
+__version__ = "0.1"
 
 #__all__ = [ 'attack', 'factory', 'metro'] # list of all classes
 from .user import User
@@ -12,3 +12,4 @@ from .pet import FightingPet
 from .pet import JoggingPet
 from .gypsy import Gypsy
 from .gift import Gift
+from .sovet import Sovet
---
+++ sovet.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
+__license__ = "MIT"
+__copyright__ = "Copyright 2014, Leonid Rusnac"
+__version__ = "0.1"
+
+import requests
+from lxml import html
+
+
+class Sovet(object):
+    def __init__(self, session, userConfig):
+        self.session = session
+        self.config = userConfig
+
+    def registerToGroup(self, lvl, password):
+        # first of all check if not blocked in some place
+        response = self.session.get(self.config['siteUrl']+'sovet/map/')
+
+        if response.url == (self.config['siteUrl']+'sovet/map/'):
+            # check if it's possible to register to game
+            tree = html.fromstring(response.text)
+            pr = tree.xpath('//span[@class="button big"]/@onclick')
+
+            if pr:
+                token = pr[0].split("'")[1]
+                # now register do game
+                self.session.post(self.config['siteUrl'] +
+                                  'sovet/join_metro_fight/', data={
+                                      'action': 'join_metro_fight',
+                                      'metro': lvl,
+                                      'type': 'metro',
+                                      'joinkey': token})
+                # now that I'm registered to fight let's enter to the group
+                self.session.post(self.config['siteUrl'] +
+                                  'groups/tryPasscode/', data={
+                                      'action': 'tryPasscode',
+                                      'type': 'sovet',
+                                      'passcode': password})
+                # now eat vitamins
+                self.session.get(self.config['siteUrl'] +
+                                 'player/json/use/1792887764/')
+            else:
+                print "can't register for sovet"
+        else:
+            print 'blocked in some place, can\'t register to sovet'
