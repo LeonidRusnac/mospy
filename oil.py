34a964c32a6a9045a24d95c48be260202ca72d9d
Leonid <leonid@reepay.com>
2015-10-22 16:45:42 +0200

    first things

--- oil.py
+++ oil.py
@@ -10,20 +10,47 @@ from lxml import html
 
 
 class Oil(object):
+
     def __init__(self, session, userConfig):
         self.session = session
         self.config = userConfig
 
     def getProgress(self):
-        print 'bazinga'
-        response = self.session.get(self.config['siteUrl'] + "neftlenin/")
+        if self.isOldOil():
+            progress = -1
+        else:
+            response = self.session.get(self.config['siteUrl'] + "neftlenin/")
 
-        tree = html.fromstring(response.text)
-        progress = tree.xpath('//div[@class="progress-wrapper"]//i[@class="counter"]/text()')[0]
+            tree = html.fromstring(response.text)
+            progress = tree.xpath(
+                '//div[@class="progress-wrapper"]//i[@class="counter"]/text()')[0]
         return progress
 
     def getCurrentLevelNumber(self):
-        pass
+        if self.isOldOil():
+            pass
+        else:
+            # $.post('http://www.moswar.ru/neftlenin', {"action": "getPrize"})
+            pass
 
     def getCurrentLevelType(self):
-        pass
+        if self.isOldOil():
+            pass
+        else:
+            pass
+
+    def isOilFinishedForToday(self):
+        return False
+
+    def canSwitchToOldOilType(self):
+        return True
+
+    def switchToOldOilType(self):
+        self.session.post(
+            self.config['siteUrl'] +
+            'neftlenin',
+            data={
+                'action': 'hideNeftLEnin'})
+
+    def isOldOil(self):
+        return False
