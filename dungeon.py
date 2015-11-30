71b86ad0ac074550ac8cfcca840a317e9a6f4731
Leonid <leonidrusnac4@gmail.com>
2015-11-30 17:33:50 +0100

    structure ready

--- dungeon.py
+++ dungeon.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__    = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
-__license__   = "MIT"
+__author__ = "Leonid Rusnac <leonidrusnac4 at gmail.com>"
+__license__ = "MIT"
 __copyright__ = "Copyright 2014, Leonid Rusnac"
-__version__   = "0.1"
+__version__ = "0.1"
 
-import requests
-from lxml import html
+# import requests
+# from lxml import html
+from enum import Enum
+
 
 class Dungeon(object):
+
+    class Type(Enum):
+        SINGLE = 's'
+        GROUP = 'g'
 
     def __init__(self, session, userConfig):
         self.session = session
         self.config = userConfig
-
