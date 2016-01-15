4cc69efc6a9fda32968978b11947fb401c13946e
Leonid <leonidrusnac4@gmail.com>
2016-01-15 17:50:11 +0100

    added the direction and rides

--- automobile.py
+++ automobile.py
@@ -10,10 +10,11 @@ import requests
 from lxml import html
 
 class Auto(object):
-    def __init__(self, session, userConfig, carid):
+    def __init__(self, session, userConfig, carid, direction='None'):
         self.session = session
         self.config = userConfig
         self.carid = carid
+        self.direction = direction
 
     def bringup(self, points=9999):
         response = self.session.get(self.config['siteUrl']+'arbat/')
@@ -23,15 +24,31 @@ class Auto(object):
             timerBringUp = tree.xpath('//table[@class="process"]//td[@id="cooldown"]/@timer')
 
             if not timerBringUp:
-                # look if enough fuel
-                response = self.session.get(self.config['siteUrl']+'automobile/car/'+str(self.carid)+'/')
-                tree = html.fromstring(response.text)
-
-                fuel = unicode(tree.xpath('//div[@class="fuel"]/span/text()'))
-                if unicode('0/5') in fuel:
-                    self.session.post(self.config['siteUrl']+'automobile/buypetrol/'+str(self.carid)+'/', data={})
-
+                self.buyPetrol()
 
                 self.session.post(self.config['siteUrl']+'automobile/bringup/', data={
                     'car': self.carid
                 })
+
+    def ride(self, direction='None'):
+        if not direction:
+            if not self.direction:
+                return
+            else:
+                direction = self.direction
+
+        self.buyPetrol()
+
+        response = self.session.post(self.config['siteUrl']+'automobile/ride/',
+                                     data = {
+                                        'direction': direction,
+                                        'car': self.carid
+                                     })
+
+    def buyPetrol(self):
+        response = self.session.get(self.config['siteUrl']+'automobile/car/'+str(self.carid)+'/')
+        tree = html.fromstring(response.text)
+
+        fuel = unicode(tree.xpath('//div[@class="fuel"]/span/text()'))
+        if unicode('0/5') in fuel:
+            self.session.post(self.config['siteUrl']+'automobile/buypetrol/'+str(self.carid)+'/', data={})
